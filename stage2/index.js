fd1ac1d971635ed9caf0aca8e3bb801967cd3407
Jonathan Pool <pool@stulta.com>
2017-08-05 22:22:34 -0700

    Added keydown listening and responding.

--- stage2/index.js
+++ stage2/index.js
@@ -27,9 +27,44 @@ var imputedText = function imputedText(element) {
 };
 
 // Define a function to respond to a button click.
-var respond = function respond(event) {
+var clickRespond = function clickRespond(event) {
   var buttonText = imputedText(event.target);
-  document.getElementById('result').innerText = buttonText;
+  document.getElementById('result').innerText += buttonText;
 };
 
-document.getElementById("buttons0").addEventListener('click', respond);
+// Define a function to respond to a keypress.
+var keyRespond = function keyRespond(event) {
+  var buttonKeys = {
+    'Clear': 'C',
+    'Escape': ['C', 'AC'],
+    'Dead': 'AC',
+    '–': '+∕−',
+    '%': '%',
+    '/': '÷',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    '*': '×',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '-': '–',
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '0': '0',
+    '.': '.',
+    '=': '=',
+  };
+  if (Object.keys(buttonKeys).includes(event.key)) {
+    var meaning = buttonKeys[event.key];
+    if (Array.isArray(meaning)) {
+      meaning = meaning[event.altKey ? 1 : 0];
+    }
+    document.getElementById('result').innerText += meaning;
+  }
+};
+
+// Event listeners
+document.getElementById('buttons0').addEventListener('click', clickRespond);
+window.addEventListener('keydown', keyRespond);
